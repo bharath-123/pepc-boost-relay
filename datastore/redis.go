9cf894377e4d39bd94ba1d33f6c809d3a37f7cba
Bharath Vedartham <vedabharath12345@gmail.com>
2023-09-12 18:44:47 +0530

    refactor

--- datastore/redis.go
+++ datastore/redis.go
@@ -89,17 +89,15 @@ type RedisCache struct {
 	prefixTobTobTx      string
 
 	// prefixes (keys generated with a function)
-	prefixGetHeaderResponse      string
-	prefixExecPayloadCapella     string
-	prefixBidTrace               string
-	prefixBlockBuilderLatestBids string // latest bid for a given slot
-
+	prefixGetHeaderResponse           string
+	prefixExecPayloadCapella          string
+	prefixBidTrace                    string
+	prefixBlockBuilderLatestBids      string // latest bid for a given slot
 	prefixBlockBuilderLatestBidsValue string // value of latest bid for a given slot
 	prefixBlockBuilderLatestBidsTime  string // when the request was received, to avoid older requests overwriting newer ones after a slot validation
 	prefixTopBidValue                 string
-
-	prefixFloorBid      string
-	prefixFloorBidValue string
+	prefixFloorBid                    string
+	prefixFloorBidValue               string
 
 	// keys
 	keyValidatorRegistrationTimestamp string
@@ -141,9 +139,8 @@ func NewRedisCache(prefix, redisURI, readonlyURI string) (*RedisCache, error) {
 		prefixBlockBuilderLatestBidsValue: fmt.Sprintf("%s/%s:block-builder-latest-bid-value", redisPrefix, prefix), // hashmap for slot+parentHash+proposerPubkey with builderPubkey as field
 		prefixBlockBuilderLatestBidsTime:  fmt.Sprintf("%s/%s:block-builder-latest-bid-time", redisPrefix, prefix),  // hashmap for slot+parentHash+proposerPubkey with builderPubkey as field
 		prefixTopBidValue:                 fmt.Sprintf("%s/%s:top-bid-value", redisPrefix, prefix),                  // prefix:slot_parentHash_proposerPubkey
-
-		prefixFloorBid:      fmt.Sprintf("%s/%s:bid-floor", redisPrefix, prefix),       // prefix:slot_parentHash_proposerPubkey
-		prefixFloorBidValue: fmt.Sprintf("%s/%s:bid-floor-value", redisPrefix, prefix), // prefix:slot_parentHash_proposerPubkey
+		prefixFloorBid:                    fmt.Sprintf("%s/%s:bid-floor", redisPrefix, prefix),                      // prefix:slot_parentHash_proposerPubkey
+		prefixFloorBidValue:               fmt.Sprintf("%s/%s:bid-floor-value", redisPrefix, prefix),                // prefix:slot_parentHash_proposerPubkey
 
 		keyValidatorRegistrationTimestamp: fmt.Sprintf("%s/%s:validator-registration-timestamp", redisPrefix, prefix),
 		keyRelayConfig:                    fmt.Sprintf("%s/%s:relay-config", redisPrefix, prefix),
